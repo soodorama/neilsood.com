18b27190eacba283e73994ebe942715d3aadccff
Neil Sood <soodorama@gmail.com>
2018-12-02 17:33:54 -0800

    parallax over multi-page for this simple of a website

--- server.py
+++ server.py
@@ -2,22 +2,23 @@ from flask import Flask, render_template, redirect
 import datetime
 app = Flask(__name__)
 
+year = datetime.datetime.now().year
+
 @app.route('/')
 def index():
-    year = datetime.datetime.now().year
     return render_template('index.html', year=year)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', year=year)
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', year=year)
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', year=year)
 
 if __name__ == "__main__":
     app.run(debug=True)
